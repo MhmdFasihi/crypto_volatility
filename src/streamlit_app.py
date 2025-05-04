a000929a53040930ee9549fb5576bcda4131ec88
MhmdFasihi <mhmdfasihi@users.noreply.github.com>
2025-05-04 16:11:11 +0330

    streamlit app update

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -5,7 +5,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 # Import from src.dashboard
-from src.dashboard import main
+from .dashboard import main
 
 if __name__ == "__main__":
     main()
